1a6db5947bbf2c86f59e5e68d5ff06f5b643a7b7
bodik@list.ru <Aq1sw2de3>
2017-06-23 16:23:39 +0200

    Planet generator

---
+++ Core/src/main/java/com/voyager/core/generator/PlanetGenerator.java
@@ -0,0 +1,16 @@
+package com.voyager.core.generator;
+
+import com.voyager.model.entity.Sector;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by volodymyr.bodnar on 6/23/2017.
+ */
+@Component
+public class PlanetGenerator {
+
+
+    public void generatePlanetsForSector(Sector sector){
+
+    }
+}
